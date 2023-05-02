cd8032b048776e84d8927395ea47f30122b7c850
Dav Hunyah <xgarhontx@gmail.com>
2023-05-02 14:47:10 -0700

    blank functions

--- JavaVer/src/Main.java
+++ JavaVer/src/Main.java
@@ -2,4 +2,57 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    /**
+     * First & Last Name
+     */
+    private static void promptUserName() {
+
+    }
+
+    /**
+     * 2 Ints
+     */
+    private static void prompt2Ints() {
+
+    }
+
+    /**
+     * Input file name/path
+     */
+    private static void promptInFileName() {
+
+    }
+
+    /**
+     * Output file name/path
+     */
+    private static void promptOutFileName() {
+
+    }
+
+    /**
+     * Password, hashed and salted
+     */
+    private static void promptPassword() {
+
+    }
+
+    /**
+     * Password verification by hashed and salted then compare
+     */
+    private static void promptPasswordRetype() {
+
+    }
+
+    /**
+     * - writes the user's name <br>
+     * - writes the result of adding the two integer values (no overflow should occur) <br>
+     * - writes the result of multiplying the two integer values (no overflow should occur) <br>
+     * - writes the contents of the input file <br>
+     * - Each thing written should be clearly labeled (e.g. First name, Last name, First Integer, Second Integer, Sum, Product, Input File Name, Input file contents)
+     */
+    private static void writeOutput() {
+
+    }
 }
--- PythonVer/src/Main.py
+++ PythonVer/src/Main.py
@@ -1,2 +1,34 @@
 if __name__ == '__main__':
     print("hello")
+
+# First & Last Name
+def promptUserName():
+    return
+
+# 2 Ints
+def prompt2Ints():
+    return
+
+# Input file name/path
+def promptInFileName():
+    return
+
+# Output file name/path
+def promptOutFileName():
+    return
+
+# Password, hashed and salted
+def promptPassword():
+    return
+
+# Password verification by hashed and salted then compare
+def promptPasswordRetype():
+    return
+
+# -writes the user's name
+# -writes the result of adding the two integer values (no overflow should occur)
+# -writes the result of multiplying the two integer values (no overflow should occur)
+# -writes the contents of the input file
+# -Each thing written should be clearly labeled (e.g. First name, Last name, First Integer, Second Integer, Sum, Product, Input File Name, Input file contents)
+def writeOutput():
+    return
