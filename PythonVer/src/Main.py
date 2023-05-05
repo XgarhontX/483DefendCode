685de0f219d8c2f6aed927efe5fbe4ac777083ca
Dav Hunyah <xgarhontx@gmail.com>
2023-05-04 22:09:07 -0700

    Python better file console

--- PythonVer/src/Main.py
+++ PythonVer/src/Main.py
@@ -96,14 +96,14 @@ def promptInFileName():
     file = None
     path = None
     while file == None:
-        print("\nSelect the input file.\n> ", end="")
+        print("\nSelect the input file\n(A File Browser was opened.)", end="")
         path = tkinter.filedialog.askopenfilename(initialdir="./", title="Select the input file.")
 
         # handle cancel
         if (path == None or path == ""):
-            print("null\nNo file selected.")
+            print("\n> null\nNo file selected.")
             continue
-        print(path)
+        print("\n> " + path)
 
         file = open(path, 'r')
         if not file.readable():
@@ -118,14 +118,14 @@ def promptOutFileName(inPath):
     file = None
     path = None
     while file == None:
-        print("\nSelect the output file.\n> ", end="")
+        print("\nSelect the output file.\n(A File Browser was opened.)", end="")
         path = tkinter.filedialog.asksaveasfilename(initialdir="./", title="Select the output file.")
 
         # handle cancel
         if (path == None or path == ""):
-            print("null\nNo file selected.")
+            print("\n> null\nNo file selected.")
             continue
-        print(path)
+        print("\n> " + path)
 
         # check if same as input
         if path == inPath:
@@ -194,14 +194,14 @@ def writeOutput():
 if __name__ == '__main__':
     print("\nPYTHON VERSION")
 
-    # nameFirst = promptNameFirst()
-    # nameLast = promptNameLast()
+    nameFirst = promptNameFirst()
+    nameLast = promptNameLast()
 
-    # int1 = prompt2Ints1()
-    # int2 = prompt2Ints2()
-    #
-    # inPath = promptInFileName()
-    # outPath = promptOutFileName(inPath)
+    int1 = prompt2Ints1()
+    int2 = prompt2Ints2()
+
+    inPath = promptInFileName()
+    outPath = promptOutFileName(inPath)
 
     promptPassword()
     promptPasswordRetype()
